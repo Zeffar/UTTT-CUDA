76c747ae22992884b2c6a76116f0add653c07d85
zeffar <george.manolachi99@gmail.com>
2024-01-29 00:16:08 +0200

    Added files, nothing interesting here

---
+++ TicTacToe.cpp
@@ -0,0 +1,25 @@
+#pragma GCC optimize("O3,unroll-loops")
+// #include <iostream>
+// #include <algorithm>
+// #include <cmath>
+// #include <fstream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    bitset<18*9> b;
+    cout<<b;
+    // string file = "gameState";
+    // ifstream read(file);
+    // srand(time(0));
+    // short a[9][9], i{}, j{};
+    // bool x;
+    // for( ; i < 10; ++i) {
+    //     for( ; j < 10; ++j) {
+    //         read>>x;
+    //         a[i][j]=x;
+    //     }
+    // }
+    
+    return 0;
+}
--- test.cu
+++ test.cu
@@ -1,68 +1,61 @@
 #include <iostream>
-#include <math.h>
-#include <chrono>
 
-// Kernel function to add the elements of two arrays
 __global__
-void add(long n, float *x, float *y)
+void saxpy(int n, float a, float *x, float *y)
 {
-  long index = blockIdx.x * blockDim.x + threadIdx.x;
-  if (index < n)
-    y[index] = x[index] + y[index];
+  int i = blockIdx.x*blockDim.x + threadIdx.x;
+  if (i < n) y[i] = a*x[i] + y[i];
 }
 
 int main(void)
 {
+  int N = 1 << 28;
+  float *x, *y, *d_x, *d_y;
+  x = (float*)malloc(N*sizeof(float));
+  y = (float*)malloc(N*sizeof(float));
 
+  cudaMalloc(&d_x, N*sizeof(float)); 
+  cudaMalloc(&d_y, N*sizeof(float));
 
-  long N = 1<<30;
-  float *x, *y;
+  for ( int i = 0; i < N; i++) {
 
-  // Allocate Unified Memory – accessible from CPU or GPU
-  cudaMallocManaged(&x, N*sizeof(float));
-  cudaMallocManaged(&y, N*sizeof(float));
-
-  // initialize x and y arrays on the host
-  for (int i = 0; i < N; i++) {
     x[i] = 1.0f;
     y[i] = 2.0f;
   }
 
+  int blockSize = 1<<9;
+  long numBlocks = (N + blockSize - 1) / blockSize;
   cudaEvent_t start, stop;
-  float elapsedTime;
-  for(int i=0; i<10; ++i){
-    cudaEventCreate(&start);
-    cudaEventRecord(start,0);
-
-    // Run kernel on 1M elements on the GPU
-    int blockSize = 1024;
-    long numBlocks = (N + blockSize - 1) / blockSize;
-    add<<<numBlocks, blockSize>>>(N, x, y);
-
-    cudaEventCreate(&stop);
-    cudaEventRecord(stop,0);
-    cudaEventSynchronize(stop);
-
-    cudaEventElapsedTime(&elapsedTime, start,stop);
-    printf("Elapsed time : %f ms\n" ,elapsedTime);
-
-    cudaEventDestroy(start);
-    cudaEventDestroy(stop);
-    // Wait for GPU to finish before accessing on host
-    cudaDeviceSynchronize();
-
-    // Check for errors (all values should be 3.0f)
-    // float maxError = 0.0f;
-    // for (int i = 0; i < N; i++)
-    //   maxError = fmax(maxError, fabs(y[i]-3.0f));
-    // std::cout << "Max error: " << maxError << std::endl;
-
-  }
-  // Free memory
-  cudaFree(x);
-  cudaFree(y);
-
 
   
-  return 0;
+
+  cudaEventCreate(&start);
+  cudaEventCreate(&stop);
+
+
+
+  for( int i = 0; i < 10; ++i) {
+    cudaMemcpy(d_x, x, N*sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_y, y, N*sizeof(float), cudaMemcpyHostToDevice);
+    cudaEventRecord(start);
+
+    // Perform SAXPY on 1M elements
+    saxpy<<<numBlocks, blockSize>>>(N, 2.0f, d_x, d_y);
+
+    cudaEventRecord(stop);
+
+    cudaMemcpy(y, d_y, N*sizeof(float), cudaMemcpyDeviceToHost);
+
+    cudaEventSynchronize(stop);
+    float milliseconds = 0;
+    cudaEventElapsedTime(&milliseconds, start, stop);
+    float maxError = 0.0f;
+    for (int i = 0; i < N; i++) {
+      maxError = max(maxError, abs(y[i]-4.0f));     
+      y[i]-=2.0f; 
+    }
+    printf("Elapsed time: %fms ", milliseconds);
+    printf("Max error: %f ", maxError);
+    printf("Effective Bandwidth (GB/s): %f\n", 1LL*N*4*3/milliseconds/1e6);
+  }
 }
